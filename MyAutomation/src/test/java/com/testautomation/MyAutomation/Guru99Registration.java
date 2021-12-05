6960b5c94a9ac7cf1f7bd5d77165bc2725e6c599
Jayu Dhengle <JayuDhengle@gmai.com>
2021-12-05 20:24:33 +0530

    commit

--- MyAutomation/src/test/java/com/testautomation/MyAutomation/Guru99Registration.java
+++ MyAutomation/src/test/java/com/testautomation/MyAutomation/Guru99Registration.java
@@ -10,8 +10,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -41,13 +39,9 @@ public class Guru99Registration
 	  System.out.println("Before Suite executed!!");
 	  System.setProperty("webdriver.chrome.driver",driverPath );
 	  driver = new ChromeDriver();  
-	  
-	  
+  } 
 
-  }
-  
-
-
+  @Ignore
   @BeforeClass
   public void ReadData() throws IOException
   {
@@ -80,8 +74,6 @@ public class Guru99Registration
 	  }
 	  
 	  wb.close();
-	  
-
   }
   
   @AfterSuite
@@ -96,18 +88,17 @@ public class Guru99Registration
   {
 	  System.out.println("Before Test executed!!");
 	  driver.navigate().to(url);
-	  driver.manage().window().maximize();
+	  driver.manage().window().maximize();  
 	  
-	  driver.findElement(By.name("uid")).sendKeys("mngr362401");
-	  driver.findElement(By.name("password")).sendKeys("anyrUjY");
+	  driver.findElement(By.name("uid")).sendKeys("mngr136913");
+	  driver.findElement(By.name("password")).sendKeys("Omn@12");
 	  driver.findElement(By.name("btnLogin")).click();
 	  String value = driver.findElement(By.xpath("//td[@style=\"color: green\"]")).getText();
-	  assertEquals(value, "Manger Id : mngr362401");
+	  assertEquals(value, "Manger Id : mngr136913");
 
   }
   
   @Test
-  @Ignore
   public void Loign()
   {
 	  System.out.println("Loign Test executed!!");
@@ -119,7 +110,6 @@ public class Guru99Registration
   public void Loign1()
   {
 	  System.out.println("Loign Test executed!!");
-	  //driver.findElement(By.xpath("//a[@href='addcustomerpage.php']")).click();
   }
   
   
--- MyAutomation/src/test/java/com/testautomation/MyAutomation/LoginPage.java
+++
@@ -1,5 +0,0 @@
-package com.testautomation.MyAutomation;
-
-public class LoginPage {
-
-}
